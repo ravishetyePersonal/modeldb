32aa5975644b104a62f4a11294e35173add22428
conradoverta <53195685+conradoverta@users.noreply.github.com>
2020-02-14 16:11:25 -0800

    Update protobuf build (#99)
    1) Use go package directly instead of the hack to fix imports
    2) Add swagger generation for gRPC gateway

--- protos/scripts/build.sh
+++ protos/scripts/build.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 set -e
-make gw
-make code
-cp -R ../gen/python/protos/ ../../client/verta/verta/_protos
+
+GO_OUTPUT="../gen/go/"
+PYTHON_OUTPUT="../gen/python/"
+
+GATEWAY_FLAGS="-I.. -I/usr/local/include -I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis"
+PYTHON_GRPC_FLAGS="--grpc_python_out=$PYTHON_OUTPUT --python_out=$PYTHON_OUTPUT"
+
+
+
+for protodir in $(find ../protos -name '*.proto' | xargs -n 1 dirname | sort | uniq)
+do
+    echo "Building $protodir for python"
+    python3 -m grpc_tools.protoc $PYTHON_GRPC_FLAGS $GATEWAY_FLAGS $protodir/*.proto
+
+    echo "Building $protodir for golang"
+    protoc $GATEWAY_FLAGS \
+		--go_out=plugins=grpc,paths=source_relative:$GO_OUTPUT \
+		--grpc-gateway_out=logtostderr=true,paths=source_relative,allow_delete_body=true:$GO_OUTPUT \
+		--swagger_out=logtostderr=true,allow_delete_body=true:$GO_OUTPUT \
+		$protodir/*.proto
+    echo ''
+done
+
+echo "Fixing python folders"
+for pythondir in $(find $PYTHON_OUTPUT -type d)
+do
+    touch $pythondir/__init__.py
+    for filename in ${pythondir}/*_pb2*; do
+        if [ -e $filename ]
+        then
+            sed -i '' 's|^from protos\.|from ...|g' $filename
+        fi
+    done
+done
+
+cp -R $PYTHON_OUTPUT/protos/ ../../client/verta/verta/_protos
--- protos/scripts/fix_imports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-declare -a dirnames=($1)
-
-for dirname in "${dirnames[@]}"; do
-    for filename in "${dirname}*_pb2*"; do
-        sed -i '' 's|^from protos\.|from ...|g' $filename
-    done
-done
