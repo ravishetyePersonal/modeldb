3df8e45d5955db4d0112bc09665ebe65561fdb07
Ravi Shetye <ravi@verta.ai>
2020-02-19 15:08:36 -0800

    generated python and go protos (#123)

--- protos/gen/go/protos/public/modeldb/versioning/VersioningService.pb.go
+++ protos/gen/go/protos/public/modeldb/versioning/VersioningService.pb.go
@@ -2411,11 +2411,11 @@ var fileDescriptor_8cadc242402e79bc = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // VersioningServiceClient is the client API for VersioningService service.
 //
@@ -2446,10 +2446,10 @@ type VersioningServiceClient interface {
 }
 
 type versioningServiceClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewVersioningServiceClient(cc grpc.ClientConnInterface) VersioningServiceClient {
+func NewVersioningServiceClient(cc *grpc.ClientConn) VersioningServiceClient {
 	return &versioningServiceClient{cc}
 }
 
