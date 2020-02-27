09df4f7400964cd946a5d15f0a61964cba305d63
Evgenii Lezhnin <eugene.lezhnin@quantumsoft.ru>
2020-02-27 16:23:22 +0700

    VR-3327 use rootTree as before to fix a bug

--- backend/src/main/java/ai/verta/modeldb/versioning/DatasetComponentDAORdbImpl.java
+++ backend/src/main/java/ai/verta/modeldb/versioning/DatasetComponentDAORdbImpl.java
@@ -96,10 +96,9 @@ public class DatasetComponentDAORdbImpl implements DatasetComponentDAO {
     List<ComponentEntity> componentEntities = new LinkedList<>();
     TreeElem rootTree = new TreeElem();
     for (BlobExpanded blob : blobsList) {
-      TreeElem treeElem = new TreeElem();
       switch (blob.getBlob().getContentCase()) {
         case DATASET:
-          processDataset(session, blob, treeElem, fileHasher, getBlobType(blob), componentEntities);
+          processDataset(session, blob, rootTree, fileHasher, getBlobType(blob), componentEntities);
           break;
         case ENVIRONMENT:
           throw new NotYetImplementedException(
@@ -109,8 +108,6 @@ public class DatasetComponentDAORdbImpl implements DatasetComponentDAO {
           throw new IllegalStateException(
               "unexpected Dataset type"); // TODO EL/AJ to throw right exceptions
       }
-
-      rootTree.children.putIfAbsent(treeElem.path, treeElem);
     }
     final InternalFolderElement internalFolderElement = rootTree.saveFolders(session, fileHasher);
     final String elementSha = internalFolderElement.getElementSha();
