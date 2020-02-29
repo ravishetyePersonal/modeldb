4732c9ed7efd1f4376ed2a241db691c3622eb333
Ravi Shetye <ravi@verta.ai>
2020-02-28 16:46:59 -0800

    trailing protos changes (#181)
    * Update VersioningService.proto
    
    * GetCommitComponent
    
    * go_package fix and generated files

--- protos/gen/go/protos/public/uac/UACService.pb.go
+++ protos/gen/go/protos/public/uac/UACService.pb.go
@@ -822,11 +822,11 @@ var fileDescriptor_02ea7ae8720d1cc7 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // UACServiceClient is the client API for UACService service.
 //
@@ -846,10 +846,10 @@ type UACServiceClient interface {
 }
 
 type uACServiceClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewUACServiceClient(cc grpc.ClientConnInterface) UACServiceClient {
+func NewUACServiceClient(cc *grpc.ClientConn) UACServiceClient {
 	return &uACServiceClient{cc}
 }
 
