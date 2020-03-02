9b057b49e8db835f1d8db49d09541303519b677a
Michael Liu <miliu@protonmail.com>
2020-03-02 13:27:38 -0800

    Add basic files for environment submodule (#192)

---
+++ client/verta/verta/environment/python.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from ..external import six
+
+from .._protos.public.modeldb.versioning import Environment_pb2 as _EnvironmentService
+
+from .. import _utils
+
+
+class Python(object):
+    def __init__(self):
+        pass
