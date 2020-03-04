6577e27d8151131bb760985cee9862e316eb437a
evgeniilezhnin <eugene.lezhnin@quantumsoft.ru>
2020-03-04 15:35:54 +0700

    VR-3473 environments hibernate entities (#202)
    * VR-3473 hibernate entities
    
    * VR-3473 add requirement entity
    
    * VR-3473 add suffix

---
+++ backend/src/main/java/ai/verta/modeldb/entities/environment/EnvironmentBlobEntity.java
@@ -0,0 +1,34 @@
+package ai.verta.modeldb.entities.environment;
+
+import ai.verta.modeldb.entities.ComponentEntity;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "environment_blob")
+public class EnvironmentBlobEntity {
+
+  public EnvironmentBlobEntity() {}
+
+  @Id
+  @Column(name = "blob_hash", nullable = false, columnDefinition = "varchar", length = 64)
+  private String blob_hash;
+
+  @Column(name = "environment_type")
+  private Integer environment_type;
+
+  @OneToOne(targetEntity = PythonEnvironmentBlobEntity.class, cascade = CascadeType.ALL)
+  @JoinColumn(name = "python_environment_blob_hash")
+  private PythonEnvironmentBlobEntity pythonEnvironmentBlobEntity;
+
+  @OneToOne(targetEntity = DockerEnvironmentBlobEntity.class, cascade = CascadeType.ALL)
+  @JoinColumn(name = "docker_environment_blob_hash")
+  private DockerEnvironmentBlobEntity dockerEnvironmentBlobEntity;
+
+}
+
---
+++ backend/src/main/java/ai/verta/modeldb/entities/environment/PythonEnvironmentBlobEntity.java
@@ -0,0 +1,30 @@
+package ai.verta.modeldb.entities.environment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "python_environment_blob")
+public class PythonEnvironmentBlobEntity {
+
+  public PythonEnvironmentBlobEntity() {}
+
+  @Id
+  @Column(name = "blob_hash", nullable = false, columnDefinition = "varchar", length = 64)
+  private String blob_hash;
+
+  @Column(name = "major")
+  private Integer major;
+
+  @Column(name = "minor")
+  private Integer minor;
+
+  @Column(name = "patch")
+  private Integer patch;
+
+  @Column(name = "suffix", columnDefinition = "varchar", length = 50)
+  private String suffix;
+}
+
